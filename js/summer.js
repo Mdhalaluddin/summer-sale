172d0115d12f79b62d2084b91041db59a5535620
Halal1122 <mdhealaluddin505@gmail.com>
2023-08-21 02:19:44 +0600

    total price added

--- js/summer.js
+++ js/summer.js
@@ -1,1 +1,20 @@
 console.log('summer asce')
+// function setInputValue (fieldId){
+//     const element = document.getElementById(fieldId);
+//     const text = parseFloat(element);
+//     const total = 
+
+// }
+document.getElementById('hlw').addEventListener('click', function(){
+    const cardAmountAdd = document.getElementById('tkAdd')
+    const cardAmountAddString = cardAmountAdd.innerText;
+    const cardAmount = parseFloat(cardAmountAddString);
+    // console.log(cardAmount);
+
+    const addedTotalPrice = document.getElementById('avarageTotalPrice');
+    const addedPriceTotal = addedTotalPrice.innerText;
+    const addPrice = parseFloat(addedPriceTotal);
+     
+    const total = cardAmount + addPrice;
+    addedPriceTotal.innerText = total;
+})
